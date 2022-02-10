602ee1671181c7bdd4310a0046edf9f5fe5ec202
MarkCrocker <71830279+MarkCrocker@users.noreply.github.com>
2022-02-10 19:40:36 -0330

    Added validation rules and routers

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,35 @@
-import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import Footer from "./components/layout/Footer";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Landing />
-      <h1>Talk To Me App</h1>
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Landing />} />
+        </Routes>
+        <div>
+          <Routes>
+            <Route exact path="/register" element={<Register />} />
+            <Route exact path="/login" element={<Login />} />
+          </Routes>
+        </div>
+        <div>
+          <Routes>
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </Routes>
+        </div>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
